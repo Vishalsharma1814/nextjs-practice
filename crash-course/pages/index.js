3e31a053ecb547eaa8c78147a52e689f35c2e35a
Vishal Sharma <sharmavishal1442000@gmail.com>
2021-07-22 20:23:21 +0530

    first commit

---
+++ crash-course/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>WEBDEV NEWS</title>
+				<meta name="keywords" content="web develppment, programming"></meta>
+			</Head>
+			<h1>Welcome to next</h1>
+		</div>
+	);
+}
